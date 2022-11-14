273707f1ccc8fda72ae452f7f48e28cbabdc9e76
Gabbum <gabbum360@gmail.com>
2022-11-14 11:56:33 +0100

    so here i moved the consumingEndpoints to its own controller and its wortking fine

--- SMS_Seperation-Of-Concerns/Controllers/StudentController.cs
+++ SMS_Seperation-Of-Concerns/Controllers/StudentController.cs
@@ -1,6 +1,10 @@
 ﻿using Business_Logic_Layer.Interfaces;
 using Core.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System;
 using System.Threading.Tasks;
 
 namespace SMS_Seperation_Of_Concerns.Controllers
@@ -16,7 +20,53 @@ namespace SMS_Seperation_Of_Concerns.Controllers
         {
             _students = students;   //initialised here.
         }
+        //the following Consume Methods are being transfered/moved to its own controller "ConsumingEndpoints" and no longer
+        //useful here...
+        /*string baseUri = "https://localhost:44353/";
 
+        [HttpGet("okay")]
+        public async Task<List<Student>> ConsumeRetrieve()
+        {
+            List<Student> students = new List<Student>();
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri(baseUri);
+            client.DefaultRequestHeaders.Clear();
+            HttpResponseMessage res = await client.GetAsync("https://localhost:44353/get-list-of-student");
+            if (res.IsSuccessStatusCode)
+            {
+                var stdResp = res.Content.ReadAsStringAsync().Result;
+                students = JsonConvert.DeserializeObject<List<Student>>(stdResp);
+            }
+            return students;
+        }
+
+        [HttpPost("posting")]
+        public async Task<Student> ConsumeSave(Student student)
+        {
+            var std = new Student()
+            {
+                Id = student.Id,
+                SurName = student.SurName,
+                FirstName = student.FirstName,
+                Sex = student.Sex,
+                Age = student.Age,
+                ClassArmId = student.ClassArmId,
+                StudentNo = student.StudentNo,
+                Country = student.Country,
+            };
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(baseUri);
+            string json = JsonConvert.SerializeObject(std);
+            StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage res = await client.PostAsync("https://localhost:44353/post-student", httpContent);
+            if (res.IsSuccessStatusCode)
+            {
+                res.EnsureSuccessStatusCode();
+            }
+            return std;
+        }
+*/
         [HttpGet("get-all-students")]
         public async Task<IActionResult> GetAllStudents()
         {
